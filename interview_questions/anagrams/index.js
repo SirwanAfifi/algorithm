c330310fb43ba9fa26f431f65ab808ebd26001f0
sirwan <sir1afifi@gmail.com>
2019-12-31 11:43:03 +0330

    Add anagrams

---
+++ interview_questions/anagrams/index.js
@@ -0,0 +1,9 @@
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g).toLowerCase().split("").sort().join("");
+}
+
+module.exports = anagrams;

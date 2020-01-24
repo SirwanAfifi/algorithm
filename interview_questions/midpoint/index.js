d032177c4c21678402b5909cbccbbadd17afcef2
sirwan <sir1afifi@gmail.com>
2020-01-24 10:31:59 +0330

    Add midpoint solution

--- interview_questions/midpoint/index.js
+++ interview_questions/midpoint/index.js
@@ -1,3 +1,13 @@
-function midpoint(list) {}
+function midpoint(list) {
+  let slow = list.getFirst(); // list.head;
+  let fast = list.getFirst();
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
 
 module.exports = midpoint;
